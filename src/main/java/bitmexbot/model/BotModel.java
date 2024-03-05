f39ef1950fb4f432932c8472fdc4d4d89abeeb08
Yulia <arven_95@mail.ru>
2024-03-05 22:04:46 +0300

    commit

--- src/main/java/bitmexbot/model/BotModel.java
+++ src/main/java/bitmexbot/model/BotModel.java
@@ -1,15 +1,26 @@
 package bitmexbot.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 
 @Data
-@Getter
+@Entity
+@Table(name = "bots")
 public class Bot {
+
+    @Id
+    private String id;
+
+    @ManyToOne
+    private User user;
 
     private double step;
     private int level;
     private double coefficient;
+
+    @Column(name = "work_indicator")
+    private boolean workingIndicator;
     private int[] sequenceFibonacci;
     private int miniLevel; //уровень контрордеров
 
